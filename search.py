1f0756970c2bee2e60188fe257c2f0c30ef9e688
Jared <jared0192galicia@gmail.com>
2022-12-02 10:13:44 -0600

    point restauration

--- search.py
+++ search.py
@@ -3,17 +3,18 @@ import os
 import time
 def main():
     sounds = os.listdir("./sound")
-    print(sounds)
+    
     
     data = open("data.txt", "w")
-    
+    i = 0
     for sound in sounds:
         if(sound[-4:] == '.wav'):
-            data.write(sound + "\n")
+            data.write(str(i) + ".- " + sound + "\n")
+            i += 1
         else:
             print(f" no: {sound}")
     data.close()
-    
+    print(sounds)
     time.sleep(1)
     
 if __name__ == '__main__':
